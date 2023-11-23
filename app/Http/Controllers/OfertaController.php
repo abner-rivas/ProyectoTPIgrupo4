041fb2a8a77ef2c41b56235e6684f956dbf93e1f
alexis1179 <castilloalex6000@gmail.com>
2023-11-22 20:47:00 -0600

    Editar y eliminar ofertas

--- app/Http/Controllers/OfertaController.php
+++ app/Http/Controllers/OfertaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use App\Models\Oferta;
 
 class OfertaController extends Controller
 {
@@ -11,18 +12,29 @@ class OfertaController extends Controller
 
     //Create
 
-    public function edit()
+    public function editar(Oferta $oferta)
     {
-        return View();
+        return View('ofertas.editar', ['oferta' => $oferta]);
     }
 
-    public function update()
+    public function actualizar(Request $request)
     {
-
+        $oferta = Oferta::find($request->id);
+        $oferta->carreras_solicitadas = $request->carreras;
+        $oferta->puesto = $request->puesto;
+        $oferta->cantidad_estudiantes = $request->estudiantes;
+        $oferta->salario = $request->salario;
+        $oferta->descripcion_proyecto = $request->descripcion;
+        $oferta->fecha_inicio = $request->fecha_inicio;
+        $oferta->fecha_fin = $request->fecha_fin;
+        $oferta->fecha_max_aplicar = $request->fecha_max;
+        $oferta->save();
+        //redirigir al index del controlador
     }
 
-    public function delete()
+    public function eliminar($id)
     {
-        return View();
+        Oferta::destroy($id);
+        //redirigir al index del controlador
     }
 }
---
+++ resources/views/ofertas/editar.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Editar Oferta</div>
+
+                <div class="card-body">
+                    @if (isset($oferta))
+                    <form action="{{route('ofertas.actualizar', $oferta->id)}}" method="post">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3">
+                            <label for="" class="form-label">Puesto</label>
+                            <input type="text" name="puesto" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{ $oferta->puesto }}">
+                            <label for="" class="form-label">Carreras Solicitadas</label>
+                            <input type="text" name="carreras" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{ $oferta->carreras_solicitadas }}">
+                            <label for="" class="form-label">Cantidad de Estudiantes</label>
+                            <input type="number" step="1" name="estudiantes" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{ $oferta->cantidad_estudiantes }}">
+                            <label for="" class="form-label">Salario</label>
+                            <input type="number" step="0.10 " name=salario"" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{ $oferta->salario }}">
+                            <label for="" class="form-label">Descripción de Proyecto</label>
+                            <textarea name="descripcion" id="" cols="80" rows="10" class="form-control">{{ $oferta->descripcion_proyecto}}</textarea>
+                            <label for="" class="form-label">Fecha de inicio</label>
+                            <input type="datetime-local" name="fecha_inicio" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{ $oferta->fecha_inicio}}">
+                            <label for="" class="form-label">Fecha de Finalizacion</label>
+                            <input type="datetime-local" name="fecha_fin" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{ $oferta->fecha_fin }}">
+                            <label for="" class="form-label">Fecha límite de aplicación</label>
+                            <input type="datetime-local" name="fecha_max" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{ $oferta->fecha_max_aplicar }}">
+                        </div>
+                        <div class="d-grid gap-2">
+                          <button type="submit" name="" id="" class="btn btn-primary">Actualizar</button>
+                        </div>
+                    </form>
+                    @else
+                    <h2 class="display-2">
+                        Oferta no encontrada!
+                    </h2>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
